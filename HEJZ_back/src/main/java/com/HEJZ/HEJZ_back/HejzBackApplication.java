582017446155892c2382158ead5ed2ebb000c49f
yanghaemi <heau0105@naver.com>
2025-05-17 05:16:17 +0900

    일단 합침

---
+++ HEJZ_back/src/main/java/com/HEJZ/HEJZ_back/HejzBackApplication.java
@@ -0,0 +1,13 @@
+package com.HEJZ.HEJZ_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HejzBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HejzBackApplication.class, args);
+	}
+
+}
